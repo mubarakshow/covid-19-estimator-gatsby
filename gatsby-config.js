003ff9bef9e274b02c032ef9ffe75e9198b7ad17
Mubarak Show <mubarak.show@gmail.com>
2020-04-15 10:45:54 +0100

    improved accessibility

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,6 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-stylus`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-less`,
     { 
       resolve: `gatsby-plugin-purgecss`,
       options: {
